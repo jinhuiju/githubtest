403dd44ad6b46cf60217918489b5939ec7e9ab6c
jinhuiju <jinhuizu@gmail.com>
2022-12-16 22:58:55 +0900

    please

---
+++ TodoApp2/src/main/java/com/example/TodoApp/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.TodoApp.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "To-do Application !";
+    }
+}
